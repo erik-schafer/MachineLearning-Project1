efbdc5570d4faf9747a1cdda19e5e9f8fdc2ca61
erik-schafer <eschafer90@gmail.com>
2018-09-19 05:30:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from sklearn import datasets
 from sklearn.datasets import load_iris
 
 from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm
+from sklearn.ensemble import GradientBoostingClassifier
 
 import graphviz 
 
@@ -22,23 +26,73 @@ def treeTester(X, Y):
     for i, pred in enumerate(clf.predict(X)):
         if not (pred == Y[i]):
             errCnt += 1
-    print("Error Rate: {0} %".format(100*errCnt/len(irisDataSet["data"])))
-    graphData = tree.export_graphviz(clf, out_file=None)
-    graph = graphviz.Source(graphData)
-    graph.render()
+    printErrRate(errCnt, len(X))
+    # graphData = tree.export_graphviz(clf, out_file=None)
+    # graph = graphviz.Source(graphData)
+    # graph.render()
     return
+
+def kNNTester(X,Y):
+    trainData, trainLabels = SampleDataSet(X, Y)
+    clf = KNeighborsClassifier(n_neighbors=len(set(Y))+1)
+    clf.fit(trainData, trainLabels)
+    errCnt = 0
+    for i, pred in enumerate(clf.predict(X)):
+        if not (pred == Y[i]):
+            errCnt += 1
+    printErrRate(errCnt, len(X))
+
+def MLPTester(X,Y):
+    trainData, trainLabels = SampleDataSet(X, Y)
+    clf = MLPClassifier(
+        solver='lbfgs', 
+        alpha=1e-5, 
+        hidden_layer_sizes=(3,8), 
+        random_state=1)
+    clf.fit(trainData, trainLabels)
+    errCnt = 0
+    for i, pred in enumerate(clf.predict(X)):
+        if not (pred == Y[i]):
+            errCnt += 1
+    printErrRate(errCnt, len(X))
+
+def SVMTester(X,Y):
+    trainData, trainLabels = SampleDataSet(X, Y)
+    clf = svm.SVC()
+    clf.fit(trainData, trainLabels)
+    errCnt = 0
+    for i, pred in enumerate(clf.predict(X)):
+        if not (pred == Y[i]):
+            errCnt += 1
+    printErrRate(errCnt, len(X))
+
+def GBCTester(X,Y):
+    trainData, trainLabels = SampleDataSet(X, Y)
+    clf = GradientBoostingClassifier(
+        n_estimators=100, 
+        learning_rate=1.0,
+        max_depth=1, 
+        random_state=0
+    )
+    clf.fit(trainData, trainLabels)
+    errCnt = 0
+    for i, pred in enumerate(clf.predict(X)):
+        if not (pred == Y[i]):
+            errCnt += 1
+    printErrRate(errCnt, len(X))
+
+def printErrRate(errCnt, n):
+    print("Error Rate: {0} %".format(100*errCnt/n))
 
 if __name__ == '__main__':
     irisDataSet = load_iris()
+    print("Tree")
     treeTester(irisDataSet["data"], irisDataSet["target"])
-
-    # trainData, trainLabels = SampleDataSet(irisDataSet["data"], irisDataSet["target"])
-
-    # clf = tree.DecisionTreeClassifier()
-    # clf = clf.fit(trainData, trainLabels)
-    # errCnt = 0
-    # for i, pred in enumerate(clf.predict(irisDataSet["data"])):
-    #     if not (pred == irisDataSet["target"][i]):
-    #         errCnt += 1
-    #     #print("{0}: {1} - {2} -- {3}".format(i, pred, irisDataSet["target"][i], pred == irisDataSet["target"][i]))
-    # print("Error Rate: {0} %".format(100*errCnt/len(irisDataSet["data"])))
+    print("kNN")
+    kNNTester(irisDataSet["data"], irisDataSet["target"])
+    print("MLP")
+    MLPTester(irisDataSet["data"], irisDataSet["target"])
+    print("SVM / SVC")
+    SVMTester(irisDataSet["data"], irisDataSet["target"])
+    print("GBC")
+    GBCTester(irisDataSet["data"], irisDataSet["target"])
